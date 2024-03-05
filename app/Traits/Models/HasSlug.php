3a51e6d221f97dae1c6975c5a009654810e7adf0
Alex <algerman.sam@gmail.com>
2024-03-05 14:56:15 +0700

    Made slugs unique

--- app/Traits/Models/HasSlug.php
+++ app/Traits/Models/HasSlug.php
@@ -4,18 +4,54 @@ declare(strict_types=1);
 namespace App\Traits\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 trait HasSlug
 {
     protected static function bootHasSlug(): void
     {
         static::creating(function (Model $model) {
-            $model->slug = $model->slug ?? str($model->{static::makeSlugFrom()})->slug() . time();
+            $model->makeSlug();
         });
     }
 
-    protected static function makeSlugFrom(): string
+    protected function makeSlug(): void
+    {
+        $slug = str($this->{$this->makeSlugFrom()})->slug()->value();
+
+        if (($newId = $this->getUniqueSlugId($slug)) > 0) {
+            $slug .= '-' . $newId;
+        }
+
+        $this->{$this->slugColumn()} = $this->slug ?? $slug;
+    }
+
+    protected function makeSlugFrom(): string
     {
         return 'title';
+    }
+
+    protected function slugColumn(): string
+    {
+        return 'slug';
+    }
+
+    private function getUniqueSlugId(string $slug): int
+    {
+        $incrementNumber = 0;
+        $slugModel = $this->newQuery()
+            ->where($this->slugColumn(),'LIKE',"{$slug}%")
+            ->latest()
+            ->first();
+
+        if ($slugModel) {
+            $incrementNumber++;
+            if ($slugModel->slugColumn() !== $slug) {
+                $lastNumber = (int) Str::replaceStart($slug . '-', '', $slugModel->slugColumn());
+                $incrementNumber = $lastNumber ? $lastNumber++ : $incrementNumber;
+            }
+        }
+
+        return $incrementNumber;
     }
 }
